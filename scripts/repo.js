b9ab73742eab529181261b9798e1bdefa50a7620
wohlfea <wohlfea@gmail.com>
2016-01-20 13:30:25 -0800

    marked done

--- scripts/repo.js
+++ scripts/repo.js
@@ -3,7 +3,7 @@
 
   repos.all = [];
 
-  // TODO: Refactor this ajax call into a get request to the proxy end point provided by server.js.
+  // DONE: Refactor this ajax call into a get request to the proxy end point provided by server.js.
   // repos.requestRepos = function(callback) {
   //   $.ajax({
   //     url: 'https://api.github.com/users/wohlfea/repos' +
