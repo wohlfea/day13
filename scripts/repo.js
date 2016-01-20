fb324b320e484c516bcc9f77ddb55029a9a7ab46
wohlfea <wohlfea@gmail.com>
2016-01-20 13:50:19 -0800

    removed old comments

--- scripts/repo.js
+++ scripts/repo.js
@@ -5,28 +5,6 @@
 
   // DONE: Refactor this ajax call into a get request to the proxy end point provided by server.js.
   // repos.requestRepos = function(callback) {
-  //   $.ajax({
-  //     url: 'https://api.github.com/users/wohlfea/repos' +
-  //           '?per_page=100' +
-  //           '&sort=updated',
-  //     type: 'GET',
-  //     headers: { 'Authorization': 'token ' + githubToken },
-  //     success: function(data, message, xhr) {
-  //       repos.all = data;
-  //     }
-  //   }).done(callback);
-  // };
-  // repos.requestRepos = function(callback) {
-  //   $.ajax({
-  //     url: '/github/users/wohlfea/repos' +
-  //           '?per_page=100' +
-  //           '&sort=updated',
-  //     type: 'GET',
-  //     success: function(data, message, xhr) {
-  //       repos.all = data;
-  //     }
-  //   }).done(callback);
-  // };
   repos.requestRepos = function(callback) {
     $.get('/github/users/wohlfea/repos?per_page=100&sort=updated', function(data){
       repos.all = data;
