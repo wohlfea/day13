49c8478cd4e4ffab1ea36e77e2cc2b08f6f8cee3
wohlfea <wohlfea@gmail.com>
2016-01-20 11:56:55 -0800

    changed the ajax call to point to server

--- scripts/repo.js
+++ scripts/repo.js
@@ -4,19 +4,29 @@
   repos.all = [];
 
   // TODO: Refactor this ajax call into a get request to the proxy end point provided by server.js.
+  // repos.requestRepos = function(callback) {
+  //   $.ajax({
+  //     url: 'https://api.github.com/users/wohlfea/repos' +
+  //           '?per_page=100' +
+  //           '&sort=updated',
+  //     type: 'GET',
+  //     headers: { 'Authorization': 'token ' + githubToken },
+  //     success: function(data, message, xhr) {
+  //       repos.all = data;
+  //     }
+  //   }).done(callback);
+  // };
   repos.requestRepos = function(callback) {
     $.ajax({
-      url: 'https://api.github.com/users/wohlfea/repos' +
+      url: '/github/users/wohlfea/repos' +
             '?per_page=100' +
             '&sort=updated',
       type: 'GET',
-      headers: { 'Authorization': 'token ' + githubToken },
       success: function(data, message, xhr) {
         repos.all = data;
       }
     }).done(callback);
   };
-
   repos.with = function(attr) {
     return repos.all.filter(function(repo) {
       return repo[attr];
