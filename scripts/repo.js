50c00fcb436f6f9e36a1ddea77abbbe8dd7a6685
wohlfea <wohlfea@gmail.com>
2016-01-20 13:28:13 -0800

    refactored .ajax to .get

--- scripts/repo.js
+++ scripts/repo.js
@@ -16,15 +16,20 @@
   //     }
   //   }).done(callback);
   // };
+  // repos.requestRepos = function(callback) {
+  //   $.ajax({
+  //     url: '/github/users/wohlfea/repos' +
+  //           '?per_page=100' +
+  //           '&sort=updated',
+  //     type: 'GET',
+  //     success: function(data, message, xhr) {
+  //       repos.all = data;
+  //     }
+  //   }).done(callback);
+  // };
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: '/github/users/wohlfea/repos' +
-            '?per_page=100' +
-            '&sort=updated',
-      type: 'GET',
-      success: function(data, message, xhr) {
-        repos.all = data;
-      }
+    $.get('/github/users/wohlfea/repos?per_page=100&sort=updated', function(data){
+      repos.all = data;
     }).done(callback);
   };
   repos.with = function(attr) {
